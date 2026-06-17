4921e8bc268305f19fb5ee7b37e135d8efc06c64
alya <dhitiialyaa@gmail.com>
2026-06-17 09:33:28 +0700

    [Tahap 5] Mengimplementasikan overriding method hitungTotalHarga pada kelas anak

--- TiketVelvet.php
+++ TiketVelvet.php
@@ -4,8 +4,8 @@ require_once 'Tiket.php';
 require_once 'koneksi/database.php';
 
 class TiketVelvet extends Tiket {
-    private $bantalSelimutPack; // Properti tambahan Tahap 4
-    private $layananButler; // Properti tambahan Tahap 4
+    private $bantalSelimutPack;
+    private $layananButler;
 
     public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket, $bantalSelimutPack, $layananButler) {
         parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket);
@@ -13,12 +13,11 @@ class TiketVelvet extends Tiket {
         $this->layananButler = $layananButler;
     }
 
-    // Fungsi Query Select Where
     public static function getById($id) {
         $database = new Database();
         $db = $database->getConnection();
 
-        $query = "SELECT * FROM tabel_tiket WHERE jenis_studio = 'Velvet'";
+        $query = "SELECT * FROM tabel_tiket WHERE id_tiket = :id AND jenis_studio = 'Velvet'";
         $stmt = $db->prepare($query);
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
@@ -38,6 +37,11 @@ class TiketVelvet extends Tiket {
             $row['bantal_selimut_pack'],
             $row['layanan_butler']
         );
+    }
+
+    // Overriding Tahap 5: Surcharge premium 50% (* 1.50)
+    public function hitungTotalHarga() {
+        return ($this->jumlah_kursi * $this->hargaDasarTiket) * 1.50;
     }
 
     public function tampilkanInfoFasilitas() {
